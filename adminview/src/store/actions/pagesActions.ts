a689323671c46b3e58d1a7386bef76cea8197045
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-07-19 22:29:15 +0300

    ok

--- adminview/src/store/actions/pagesActions.ts
+++ adminview/src/store/actions/pagesActions.ts
@@ -8,7 +8,7 @@ export const fetchPages = () => {
     const {fetching} =  pageSLice.actions
     return async (dispatch:AppDispatch) =>{
         try{
-            dispatch(pagesSlice.actions)
+            dispatch(fetching)
             const data = await makeRequest('https://openexchangerates.org/api/latest.json?app_id=771a6bee690546a8829f7887b24585c9','GET')
             console.log('data',data)
         
