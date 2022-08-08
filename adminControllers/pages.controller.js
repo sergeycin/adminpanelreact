d4fa298a8718393f51750d65041c898a1745c976
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-08-08 20:40:49 +0300

    pages

--- adminControllers/pages.controller.js
+++ adminControllers/pages.controller.js
@@ -99,16 +99,53 @@ var CurrentPage; // Текущая используемая страница
 
   router.post(
     '/sendimage', 
-  async (req,res) =>{
-    try{
-      const requestField = req.body
-      // console.log(requestField)
-     console.log(requestField)
-          res.json(requestField)
-          
-    }catch(e){
-      res.status(500).json({message: "Что-то пошло не так"})      
-    }
-  })
+    async (req, res) =>{
+      try {
+          const file = req.files.file
+
+          const parent = await File.findOne({user: req.user.id, _id: req.body.parent})
+          const user = await User.findOne({_id: req.user.id})
+
+          if (user.usedSpace + file.size > user.diskSpace) {
+              return res.status(400).json({message: 'There no space on the disk'})
+          }
+
+          user.usedSpace = user.usedSpace + file.size
+
+          let path;
+          if (parent) {
+              path = `${config.get('filePath')}\\${user._id}\\${parent.path}\\${file.name}`
+          } else {
+              path = `${config.get('filePath')}\\${user._id}\\${file.name}`
+          }
+
+          if (fs.existsSync(path)) {
+              return res.status(400).json({message: 'File already exist'})
+          }
+          file.mv(path)
+
+          const type = file.name.split('.').pop()
+          const dbFile = new File({
+              name: file.name,
+              type,
+              size: file.size,
+              path: parent?.path,
+              parent: parent?._id,
+              user: user._id
+          })
+
+          await dbFile.save()
+          await user.save()
+
+          res.json(dbFile)
+      } catch (e) {
+          console.log(e)
+          return res.status(500).json({message: "Upload error"})
+      }
+  }
+
+  
+  
+  )
 
 module.exports = router
