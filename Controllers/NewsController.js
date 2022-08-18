07b27cdd6ed7b91ebde0b50a3c0da3b32720c897
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-08-18 21:40:03 +0300

    newscontroller.js

--- Controllers/NewsController.js
+++ Controllers/NewsController.js
@@ -10,22 +10,38 @@ router.get(
       let resultObject = {}
        
     dataNews = await News.find({})
+    var newobj = []
     for(let data of dataNews){
     
         for(let key of Object.keys(data.schema.paths)){
            
             if(!String(key).includes("en")){
-              resultData(key)
+              // console.log(key)
+              let object = {}
+              object = JSON.parse(data);
+              delete object[`${key}`]
+              console.log(object)
+              // resultData(key)
             }
         }
       
     }
-
+    // const found_names = dataNews.filter(v => v.entitle != "Joe" && v.age < 30);
+  
     function resultData(key){
+
       for(let data of dataNews){
-        console.log(data[`${key}`])
+       
+        delete data[`${key}`]
+         newobj = Object.assign({},newobj,data);
+        
+     
+      
       }
     }
+   
+  
+    // console.log(newArray)
     res.json(dataNews)
     }catch(e){
         console.log(e)
